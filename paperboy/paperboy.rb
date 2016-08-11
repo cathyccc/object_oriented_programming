0796e8c57b93240b702dc80a5c6f34e9708ad5b6
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2016-08-11 16:13:47 -0400

    created counter for odd/even houses

--- paperboy/paperboy.rb
+++ paperboy/paperboy.rb
@@ -14,7 +14,32 @@ class Paperboy
     return @experience/2 + 50
   end
 
-  
+  def deliver(start_address,end_address)
+    # remember: 2 counters
+    # i) going up address
+    # ii) counting even houses
+
+    odd_houses = 0
+    even_houses = 0
+    total_houses = 0
+    # counter ii) start_address = start_address
+
+    while start_address <= end_address
+      if start_address % 2 == 0
+        even_houses += 1
+      else
+        odd_houses += 1
+      end
+      start_address += 1
+    end
+
+    # separates even vs. odd houses for calculation
+    if @side == "even"
+      total_houses = even_houses
+    else
+      total_houses = odd_houses
+    end
+  end
 end
 
 tommy = Paperboy.new("Tommy",0,"odd",1000)
