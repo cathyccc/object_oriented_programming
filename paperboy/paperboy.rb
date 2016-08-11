9fee8703592609f7f4d5a7bbb697e49d0e2abc4b
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2016-08-11 14:21:15 -0400

    defined instance variables

---
+++ paperboy/paperboy.rb
@@ -0,0 +1,13 @@
+class Paperboy
+
+  attr_accessor :name, :experience, :side
+  attr_reader :earnings
+
+  def initialize(name,experience,side,earnings)
+    @name = name
+    @experience = experience
+    @side = side
+    @earnings = earnings
+  end
+
+end
