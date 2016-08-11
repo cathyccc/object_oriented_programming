b1312acb44209b139c43abc78a11173098254fc0
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2016-08-11 14:11:39 -0400

    added instances to Cats & edited eats_at method

--- cat.rb
+++ cat.rb
@@ -3,14 +3,18 @@ class Cat
   attr_accessor :name, :preferred_food, :meal_time
 
   def initialize(name, preferred_food, meal_time)
-    @name = name
-    @preferred_food = preferred_food
+    @name = name.capitalize
+    @preferred_food = preferred_food.downcase
     @meal_time = meal_time
   end
 
   def eats_at
-    if @meal_time <= 11
+    if @meal_time < 12 && @meal_time > 0
       return "#{@meal_time} AM"
+    elsif @meal_time == 12
+      return "#{@meal_time} PM"
+    elsif @meal_time == 0
+      return "#{@meal_time+12} AM"
     else
       return "#{@meal_time-12} PM"
     end
@@ -21,3 +25,9 @@ class Cat
   end
 
 end
+
+garfield = Cat.new("garfield", "bacon", 12)
+arlene = Cat.new("arlene", "tuna", 0)
+
+puts garfield.meow
+puts arlene.meow
