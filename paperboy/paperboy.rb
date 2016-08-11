4dcb6bfe5d04682495e0868954fea1a1739468cd
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2016-08-11 16:11:16 -0400

    initialized instance variables

--- paperboy/paperboy.rb
+++ paperboy/paperboy.rb
@@ -3,11 +3,19 @@ class Paperboy
   attr_accessor :name, :experience, :side
   attr_reader :earnings
 
-  def initialize(name,experience,side,earnings)
+  def initialize(name, experience, side, earnings)
     @name = name
     @experience = experience
-    @side = side
+    @side = side.downcase
     @earnings = earnings
   end
 
+  def quota
+    return @experience/2 + 50
+  end
+
+  
 end
+
+tommy = Paperboy.new("Tommy",0,"odd",1000)
+tommy.deliver(2,7)
