6e5fd98fac09b1cac4fbd755ffcced84ae773cb9
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2016-08-12 09:36:27 -0400

    revised instructions to respond to each movement

--- rover/rover.rb
+++ rover/rover.rb
@@ -6,15 +6,33 @@ class Rover
     @x = x
     @y = y
     @direction = direction
+    @total_moves = []
   end
 
   #how to move and turn
   def read_instructions
-    puts "How would you like rover 1 to move?"
-    puts "'L' / 'R' - to rotate rover 90 degrees left or right respectively"
-    puts "'M' - to move forward one grid point and maintain the same heading"
-    instructions = gets.chomp
+    puts "How would you like rover to move?"
+    instructions = gets.chomp.upcase
+    dir_array = instructions.split("") # => ["L", "L", "M", "M"]
+    puts dir_array.length
+
+    counter = 0
+    while counter < dir_array.length
+      dir_array.each do |movement|
+        if movement == "L"
+          puts "rover moves left" # placeholder
+        elsif movement == "R"
+          puts "rover moves right" # placeholder
+        else
+          puts "rover moves forward" # placeholder
+        end
+      counter += 1
+      end
+    end
   end
-    
 
 end
+
+#testing
+a = Rover.new(1,2,"N")
+a.read_instructions
