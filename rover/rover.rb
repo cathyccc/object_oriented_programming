d0d534624f4e7f6b8fdd8ed6b188068daf6ce874
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2016-08-11 21:47:18 -0400

    revised instance variables; added read_instructions method

--- rover/rover.rb
+++ rover/rover.rb
@@ -2,8 +2,19 @@ class Rover
 
   attr_accessor :location, :direction
 
-  def initialize(location,direction)
-    @location = location
+  def initialize(x,y,direction)
+    @x = x
+    @y = y
     @direction = direction
   end
+
+  #how to move and turn
+  def read_instructions
+    puts "How would you like rover 1 to move?"
+    puts "'L' / 'R' - to rotate rover 90 degrees left or right respectively"
+    puts "'M' - to move forward one grid point and maintain the same heading"
+    instructions = gets.chomp
+  end
+    
+
 end
