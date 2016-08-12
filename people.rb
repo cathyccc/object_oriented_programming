e1def116f33c53b18a2173e87747692932295dc7
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2016-08-12 14:19:59 -0400

    created class method greetin

--- people.rb
+++ people.rb
@@ -1,11 +1,31 @@
-class Student
+class Person
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def greeting
+    puts "Hi, my name is #{@name}"
+  end
+
+end
+
+class Student < Person
   def learn
     return "I get it!"
   end
 end
 
-class Instructor
+class Instructor < Person
   def teach
     return "Everything in Ruby is an Object"
   end
 end
+
+chris = Instructor.new("Chris")
+cristina = Student.new("Cristina")
+
+chris.greeting
+cristina.greeting
