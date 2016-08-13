338b9427695b4fd1987f4cb59ffb14cb3fdfac20
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2016-08-13 14:33:10 -0400

    created new method for calculating different cases

--- sales_taxes/item.rb
+++ sales_taxes/item.rb
@@ -1,10 +1,49 @@
-class item
-  attr_accessor :price, :item_name, :imported, :type
+class Item
+  attr_accessor :price, :item_name, :import, :type,
 
-  def initiatize(item_name,price,type)
+  def initialize(item_name,type,price,import)
     @item_name = item_name
     @price = price
-    @type = type
-    @import = false
+    @type = type.downcase
+    @import = import
+    @bfm = false
+  end
+
+  def purchase
+    puts "Item: #{@item_name}  Price: #{@price}  Type: #{@type}  Import: #{@import}"
+  end
+
+  def after_tax
+    if @type == "book" || @type == "food" || @type == "med" || @type == "medicine" || @type == "medical" || @type == "books"
+      @bfm = true
+    else
+      @bfm = false
+    end
+
+    if @bfm == true && @import == false
+      item_taxed = @price.round(2)
+      puts "$ #{item_taxed}"
+    elsif @bfm == false && @import == false
+      item_taxed = (@price *= 1.10).round(2)
+      puts "$ #{item_taxed}"
+    elsif @bfm == true && @import == true
+      item_taxed =(@price *= 1.05).round(2)
+      puts "$ #{item_taxed}"
+    elsif @bfm == false && @import == true
+      item_taxed = (@price *= 1.15).round(2)
+      puts "$ #{item_taxed}"
+    end
   end
 end
+
+
+#testing
+book = Item.new("magazine","book",1,false)
+shirt = Item.new("pants","clothing",1,false)
+cake = Item.new("birthday cake","food",1,true)
+pen = Item.new("pen","stationary",1,true)
+
+book.after_tax
+shirt.after_tax
+cake.after_tax
+pen.after_tax
